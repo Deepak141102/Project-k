11333d8b8eb49a8ab244188f69a68ebb66b3671b
Deepak141102 <d84255411@gmail.com>
2024-09-02 17:15:25 +0530

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"], // Adjust this line according to your project structure
+  content: [
+    "./*.html",          // Ensure this includes your HTML files
+    "./src/**/*.{js,jsx,ts,tsx}", // Includes JavaScript/TypeScript files
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mochiypop: ["Mochiy Pop P One", "sans-serif"],
+      },
+      backgroundPosition: {
+        "-center-custom": "-25rem -340px",
+      },
+      
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        'xs': '480px',  // Custom breakpoint for extra small screens
+      },
+    },
   },
   plugins: [],
 };
